c22a05dee2ba34ce656628e62d04b539d3aac25c
ykmt <ykmt@example.com>
2015-03-06 16:11:56 +0900

    .txtのmark-down-modeを無効

--- inits/21-markdown-mode.el
+++ inits/21-markdown-mode.el
@@ -1,6 +1,5 @@
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
