a801a2344bd016567087cb16da4d0ca65ddbda60
ykmt <ykmt@example.com>
2015-03-10 19:52:48 +0900

    window-modeの設定を分割

--- inits/00-settings.el
+++ inits/00-settings.el
@@ -1,19 +1,3 @@
-;; theme
-(load-theme 'wombat t)
-
-;; font settings
-(set-default-font "Inconsolata-11")
-(set-face-font 'default "Inconsolata-11")
-(set-face-font 'font-lock-comment-face       "Inconsolata-11")
-(set-face-font 'font-lock-string-face        "Inconsolata-11")
-(set-face-font 'font-lock-keyword-face       "Inconsolata-11")
-(set-face-font 'font-lock-builtin-face       "Inconsolata-11")
-(set-face-font 'font-lock-function-name-face "Inconsolata-11")
-(set-face-font 'font-lock-variable-name-face "Inconsolata-11")
-(set-face-font 'font-lock-type-face          "Inconsolata-11")
-(set-face-font 'font-lock-constant-face      "Inconsolata-11")
-(set-face-font 'font-lock-warning-face       "Inconsolata-11")
-
 ;; disable startup mesage
 (setq inhibit-startup-message t)
 
