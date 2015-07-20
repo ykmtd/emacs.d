edbaa8b0ad6bb28c939fbf41254d22798e35eac1
ykmt <ykmt@example.com>
2015-07-20 11:53:18 +0900

    pbcopyで emacsとクリップボード間の共有 (OS X)

--- inits/11-clipboard.el
+++ inits/11-clipboard.el
@@ -1,19 +1,35 @@
 ;; clipboard
 
-(defun my-cut-function (text &optional rest)
+(defun cut-with-xclip (text &optional rest)
   (interactive)
   (let ((process-connection-type nil))
     (let ((proc (start-process "xclip" "*Messages*" "xclip" "-select" "clipboard")))
       (process-send-string proc text)
       (process-send-eof proc))))
 
-(defun my-paste-function ()
+(defun paste-with-xclip ()
   (interactive)
   (shell-command-to-string "xclip -o -select clipboard"))
 
+(defun cut-with-pbcopy (text &optional rest)
+  (interactive)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(defun paste-with-pbpaste ()
+  (interactive)
+  (shell-command-to-string "pbpaste"))
+
 (when (and (not window-system)
            (executable-find "xclip"))
-  (setq interprogram-cut-function 'my-cut-function)
-  (setq interprogram-paste-function 'my-paste-function))
+  (setq interprogram-cut-function 'cut-with-xclip)
+  (setq interprogram-paste-function 'paste-with-xclip))
+
+(when (and (not window-system)
+           (executable-find "pbcopy"))
+  (setq interprogram-cut-function 'cut-with-pbcopy)
+  (setq interprogram-paste-function 'paste-with-pbpaste))
 
 (if window-system (setq x-select-enable-clipboard t))
