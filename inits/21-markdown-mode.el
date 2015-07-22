d608279b33b91eb1b6d7d70f5f5d55903373cbde
ykmt <ykmt@example.com>
2015-07-23 07:22:22 +0900

    markdown のquickrun，自動pdf生成を追加

--- inits/21-markdown-mode.el
+++ inits/21-markdown-mode.el
@@ -3,3 +3,7 @@
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-hook 'markdown-mode-hook '(lambda ()
+                                 (when (executable-find "gimli")
+                                   (add-hook 'after-save-hook 'quickrun nil 'make-it-local))))
--- inits/41-quickrun.el
+++ inits/41-quickrun.el
@@ -2,10 +2,21 @@
 (require 'quickrun)
 (push '("*quickrun*" :stick t) popwin:special-display-config)
 
-(quickrun-add-command "iverilog"
-                      '((:command . "iverilog")
-                        (:exec . ("%c -o %e %s"
-                                  "%e"))
-                        (:remove . "%e")
-                        (:description . ("Compile Verilog file with iverilog and execute")))
-                      :mode 'verilog-mode)
+(when (executable-find "iverilog")
+  (quickrun-add-command "iverilog"
+                        '((:command . "iverilog")
+                          (:exec . ("%c -o %e %s"
+                                    "%e"))
+                          (:remove . "%e")
+                          (:description . ("Compile Verilog file with iverilog and execute")))
+                        :mode 'verilog-mode))
+
+(when (executable-find "gimli")
+  (quickrun-add-command "markdown-gimli"
+                        '((:command . "gimli")
+                          (:exec . ("%c"))
+                          (:tempfile . nil)
+                          (:outputter . null)
+                          (:description . ("Convert markdown to pdf with gimli")))
+                        :mode 'markdown-mode)
+  (quickrun-set-default "markdown" "markdown-gimli"))
