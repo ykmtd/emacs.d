7924b26ec5d0ec9f1ff38a9781f5208511725318
ykmt <ykmt@example.com>
2016-04-09 23:05:22 +0900

    enable hs-minor-mode in ruby-mode

--- inits/21-ruby-mode.el
+++ inits/21-ruby-mode.el
@@ -11,4 +11,17 @@
              (electric-pair-mode t)
              (add-to-list 'electric-pair-pairs '(?| . ?|))
              (electric-indent-mode t)
+             (hs-minor-mode 1)
+             (define-key hs-minor-mode-map (kbd "C-l C-l") 'hs-toggle-hiding)
              ))
+
+(let ((ruby-mode-hs-info
+       '(ruby-mode
+         "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do"
+         "end"
+         "#"
+         ruby-move-to-block
+         nil)))
+  (if (not (member ruby-mode-hs-info hs-special-modes-alist))
+      (setq hs-special-modes-alist
+                        (cons ruby-mode-hs-info hs-special-modes-alist))))
